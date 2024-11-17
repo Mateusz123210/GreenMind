64c438c654544cf5942a4aa24015738f29709803
Tomasz Filip <tomfilip1@gmail.com>
2024-11-17 14:39:31 +0100

    init fastAPI

---
+++ main.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+from fastapi import FastAPI
+
+app=FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
