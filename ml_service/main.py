a2f1b8ffecd05b07343853656aa9eddc91307ded
Tomasz Filip <tomfilip1@gmail.com>
2024-11-18 11:49:33 +0100

    add weather api connection method, add weather endpoint

--- ml_service/main.py
+++ ml_service/main.py
@@ -6,11 +6,22 @@ This is a temporary script file.
 """
 
 from fastapi import FastAPI
+import random, json
+import weather_api
 
 app=FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "GreenMind"}
 
+@app.get("/weather")
+async def weather(date: str = "", lat: float = 0, lon: float=0): 
+    return weather_api.get_weather_forecast(lat, lon, date)
+
+@app.get("/prediction")
+async def water_usage_prediction():
+    data = {}
+    data["random"]= random.random()
+    return data
 
