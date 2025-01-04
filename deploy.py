c03740113f5d895c3b244a9f15431c6a28c4d7fd
Mateusz123210 <mateusz.urbanczyk11@gmail.com>
2025-01-04 23:01:22 +0100

    Add azure kubernetes deploy script

--- deploy.py
+++ deploy.py
@@ -1,4 +1,4 @@
 import os
 os.system("docker build . -t backend2")
-os.system("docker tag backend2 thingssensorsservice.azurecr.io/backend2:latest")
-os.system("docker push thingssensorsservice.azurecr.io/backend2:latest")
+os.system("docker tag backend2 kubregistry1.azurecr.io/backend2:latest")
+os.system("docker push kubregistry1.azurecr.io/backend2:latest")
