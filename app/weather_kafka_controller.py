603ede6f0060e9d4341ea85544c345d0ef1c615d
Mateusz123210 <mateusz.urbanczyk11@gmail.com>
2025-01-04 23:18:34 +0100

    Fix kafka ip

--- app/weather_kafka_controller.py
+++ app/weather_kafka_controller.py
@@ -11,7 +11,7 @@ class WeatherKafkaController:
     def __init__(self):
 
         self.weather_fetch_time = 360000
-        self.producer = Producer({"bootstrap.servers": "20.254.227.50:9092"})
+        self.producer = Producer({"bootstrap.servers": "20.215.41.25:9092"})
         self.working = True
         self.delivery_report = None
 
