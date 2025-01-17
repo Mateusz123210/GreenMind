b93c748e4e97ff8224b69069851498c69c920d49
Mateusz123210 <mateusz.urbanczyk11@gmail.com>
2025-01-17 20:52:42 +0100

    Fixes in auth service

--- frontend/src/services/auth.ts
+++ frontend/src/services/auth.ts
@@ -66,9 +66,9 @@ export const login = (email: string, password: string) => {
         });
 };
 
-export const refreshTokens = () => {
+export const refreshTokens = async () => {
     const url = new URL(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/refresh-token`);
-    url.searchParams.append("refresh_token", localStorage.getItem("refresh_token")!);
+    url.searchParams.append("refreshToken", localStorage.getItem("refresh_token")!);
     url.searchParams.append("email", localStorage.getItem("email")!);
     return fetch(url, {
         method: "POST",
@@ -79,20 +79,61 @@ export const refreshTokens = () => {
             updateStorage("email", data.email);
             updateStorage("access_token", data.access_token);
             updateStorage("refresh_token", data.refresh_token);
+            return { email: data.email, access_token: data.access_token, refresh_token: data.refresh_token};
+
         });
+
 };
 
-export const logout = () => {
+export const logout = async () => {
     var url = new URL(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/logout`);
-    const accessToken = localStorage.getItem("access_token")!
-    const email = localStorage.getItem("email")!
+    var accessToken = localStorage.getItem("access_token")!
+    var email = localStorage.getItem("email")!
     url.searchParams.append("accessToken", accessToken);
     url.searchParams.append("email", email);
 
-    fetch(url , { method: "POST" })
+    const response = await fetch(url , { method: "POST" });
+    if (response.status == 401) {
+        const refreshTokenResponse = await refreshTokens();
+        url = new URL(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/logout`);
+        accessToken = refreshTokenResponse!["access_token"]
+        email = refreshTokenResponse!["email"]
+        url.searchParams.append("accessToken", accessToken);
+        url.searchParams.append("email", email);
+        await fetch(url , { method: "POST" });
+        localStorage.clear();
+        window.dispatchEvent(new Event("storage"));
+    }
+    else {
+        localStorage.clear();
+        window.dispatchEvent(new Event("storage"));
+    }
+    
+};
 
-    localStorage.clear();
-    window.dispatchEvent(new Event("storage"));
+export const deleteAccount = async () => {
+    var url = new URL(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/account`);
+    var accessToken = localStorage.getItem("access_token")!
+    var email = localStorage.getItem("email")!
+    url.searchParams.append("accessToken", accessToken);
+    url.searchParams.append("email", email);
+
+    const response = await fetch(url , { method: "DELETE" });
+    if (response.status == 401) {
+        const refreshTokenResponse = await refreshTokens();
+        url = new URL(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/account`);
+        accessToken = refreshTokenResponse!["access_token"]
+        email = refreshTokenResponse!["email"]
+        url.searchParams.append("accessToken", accessToken);
+        url.searchParams.append("email", email);
+        await fetch(url , { method: "DELETE" });
+        localStorage.clear();
+        window.dispatchEvent(new Event("storage"));
+    }
+    else {
+        localStorage.clear();
+        window.dispatchEvent(new Event("storage"));
+    }
 };
 
 export const updateStorage = (key: string, value: string) => {
