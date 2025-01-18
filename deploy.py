e8520cc798801ca3dfa3b0143713c36b8b181332
Mateusz123210 <mateusz.urbanczyk11@gmail.com>
2025-01-18 20:55:59 +0100

    Change kubregistry1 to kubregistry2

--- deploy.py
+++ deploy.py
@@ -1,4 +1,4 @@
 import os
 os.system("docker build . -t backend5")
-os.system("docker tag backend5 kubregistry1.azurecr.io/backend5:latest")
-os.system("docker push kubregistry1.azurecr.io/backend5:latest")
+os.system("docker tag backend5 kubregistry2.azurecr.io/backend5:latest")
+os.system("docker push kubregistry2.azurecr.io/backend5:latest")
